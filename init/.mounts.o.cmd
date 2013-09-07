e95a5acfc5d77b68ced8f07cb40d2da3e4a9a6d2
dm47021 <dm47021@hotmail.com>
2013-09-07 14:07:47 -0400

    WHY THE FUCK WAS THIS SHIT MISSING IN THE FIRST PLACE !!!!

---
+++ init/.mounts.o.cmd
@@ -0,0 +1,1 @@
+cmd_init/mounts.o := /home/dm47021/Android/toolchains/arm-eabi-4.4.3/bin/arm-eabi-ld -EL    -r -o init/mounts.o init/do_mounts.o init/do_mounts_initrd.o 
---
+++ ipc/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_ipc/built-in.o :=  rm -f ipc/built-in.o; /home/dm47021/Android/toolchains/arm-eabi-4.4.3/bin/arm-eabi-ar rcs ipc/built-in.o
